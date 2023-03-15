5a4cc476b7c1c0b8307f3121bb0118f3eb927cf0
WbrJr <info@moritz-weber.de>
2023-03-15 20:38:37 +0100

    added basic mb tests
    this adds ws2812b LED tests and basic serial comunication to check if the board works as expected

---
+++ dh_mb-firmware/Tests/serial-test/serial-test.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Serial.println("Hello there");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Oh hi Mark!");
+  delay(1000);
+}
